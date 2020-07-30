aa8ccada69197c8da2abf471cf02c4ee5e3c4243
Caio S. Batista <caio.batista@nave.rs>
2020-07-30 00:42:39 -0300

    Formulário de cadastro de categoria desenvolvido

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,13 +6,28 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria</h1>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
 
       <Link to="/">
         Ir para home
-        </Link>
+      </Link>
     </PageDefault>
-  );
+  )
 }
 
 export default CadastroCategoria;
